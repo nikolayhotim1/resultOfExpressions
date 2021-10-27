5f553536805a5428f3616c5c30b09462f3402014
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-10-27 22:41:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+let tempC = 10;
+console.log((9 / 5) * tempC + 32); // 50
+
+console.log('Number' + ' ' + '2'); // Number 2
+
+let level = 10;
+console.log(level >= 5); // true
+
+level = 5;
+console.log(level >= 5); // true
+
+let color = 'blue';
+console.log(color != 'pink'); // true
+
+let r = 3;
+console.log((2 * Math.PI) * r); // 18.84...
+
